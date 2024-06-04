b8cabd4434e1ee634e536fe7f8316eb1259dfe2c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-04 19:45:54 +0600

    Installed recharts

--- src/layout/Welcome/Welcome.jsx
+++ src/layout/Welcome/Welcome.jsx
@@ -1,6 +1,6 @@
 const Welcome = () => {
   return (
-    <div className="card card-compact container flex items-center rounded-xl mx-auto max-w-fit bg-base-100">
+    <div className="card card-compact container flex rounded-xl mx-auto max-w-fit bg-base-100">
       <h2 className="font-bold text-center text-5xl text-green-900">
         Welcome To Dashboard
       </h2>
