0f026ac23f418ead25398ee4102f1943fbcd9e50
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-09 14:54:28 +0600

    Added layout change

--- src/components/AvailableCamps/AvailCamps.jsx
+++ src/components/AvailableCamps/AvailCamps.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import useCamp from "../../hooks/useCamp";
 import AllCampCard from "./AllCampCard";
 import { useForm } from "react-hook-form";
+import { IoGridSharp } from "react-icons/io5";
+import { BsGrid3X2GapFill } from "react-icons/bs";
 
 const AvailCamps = () => {
   const [search, setSearch] = useState("");
   const [camps, refetch, loading] = useCamp(search);
+  const [threeGrid, setThreeGrid] = useState(true);
+  const [twoGrid, setTwoGrid] = useState(false);
   const {
     register,
     handleSubmit,
@@ -29,6 +33,16 @@ const AvailCamps = () => {
     // console.log(data.searchText);
   };
 
+  const handleThreeGrid = () => {
+    setTwoGrid(false);
+    setThreeGrid(true);
+  };
+
+  const handleTwoGrid = () => {
+    setThreeGrid(false);
+    setTwoGrid(true);
+  };
+
   return (
     <div className="container p-6 mx-auto">
       <div className="flex justify-center">
@@ -46,7 +60,28 @@ const AvailCamps = () => {
           />
         </form>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3">
+      <div className="flex justify-end items-center gap-4 mb-5">
+        <h2 className="font-bold">Change Layout</h2>
+        <span
+          className={`${threeGrid && "text-green-400"}`}
+          onClick={handleThreeGrid}
+        >
+          <BsGrid3X2GapFill className="text-xl" />
+        </span>
+        <span
+          className={`${twoGrid && "text-green-400"}`}
+          onClick={handleTwoGrid}
+        >
+          <IoGridSharp />
+        </span>
+      </div>
+      <div
+        className={`${
+          threeGrid
+            ? "grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3"
+            : "grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-2"
+        }`}
+      >
         {camps.map((camp) => (
           <AllCampCard key={camp._id} camp={camp}></AllCampCard>
         ))}
